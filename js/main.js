a543f57fcdcaaaeb183a97193e3aa5d0a243d027
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-10-05 16:55:06 +0600

    frequency-asked-question'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-// change navbar styles on scroll
+//! change navbar styles on scroll
 
 window.addEventListener("scroll", () => {
-  document
-    .querySelector("nav")
-    .classList.toggle("window-scroll", window.scrollY > 0);
+    document
+        .querySelector("nav")
+        .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+//!show/hide faq answer
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // icon change
+        const icon = faq.querySelector('.faq__icon i');
+
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus';
+        } else {
+            icon.className = 'uil uil-plus';
+        }
+    })
+})
