7379bb1e88744c03dccb405a2364cdef2ec15d03
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-10-14 09:39:18 +0600

    navbar

--- js/main.js
+++ js/main.js
@@ -8,23 +8,36 @@ window.addEventListener("scroll", () => {
 
 //!show/hide faq answer
 const faqs = document.querySelectorAll(".faq");
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open");
 
         // icon change
-        const icon = faq.querySelector('.faq__icon i');
+        const icon = faq.querySelector(".faq__icon i");
 
-        if (icon.className === 'uil uil-plus') {
-            icon.className = 'uil uil-minus';
+        if (icon.className === "uil uil-plus") {
+            icon.className = "uil uil-minus";
         } else {
-            icon.className = 'uil uil-plus';
+            icon.className = "uil uil-plus";
         }
-    })
-})
+    });
+});
 
 // !show/hide menu
 
 const menu = document.querySelector(".nav_menu");
 const menuBtn = document.querySelector("#open-menu-btn");
 const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+});
+
+//close nav button
+closeBtn.addEventListener('click', () => {
+    menu.style.display = "none";
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'flex';
+})
